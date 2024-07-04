8604e66cdc44b6beaa5ec2c102e4866f6f018d95
“Yash <kalpesh.tita@einfochips.com>
2024-07-04 19:48:44 +0530

    Initial Commit

---
+++ Test Demo/Test_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_DemoApp.swift
+//  Test Demo
+//
+//  Created by Yash Parikh on 04/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
